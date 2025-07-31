8fc8b4cec7e37f09e19fb4fe8cb0e8d27013953d
Jonathan Oudshoorn <jonathan.oudshoorn@ns.nl>
2025-07-31 15:44:21 +0200

    seperated the DB's from the pods

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ skaffold dev
 
 # cleanup unused docker images (created by skaffold)
 # docker image prune -af
+
+# to run the jobs:
+# kubectl apply -f k8s/jobs/questiondb-seed-job.yaml
+# kubectl apply -f k8s/jobs/questiondb-wipe-job.yaml
